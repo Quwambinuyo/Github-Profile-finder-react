2d14c729c0ad2c556399ce7f8ff5083ea5feab1b
Quwam Binuyo <qbinuyo7@gmail.com>
2025-01-10 14:02:03 +0100

    created the navbar component and styled it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
-  return <div className="bg-purple-500"></div>;
+  return (
+    <Router>
+      <div className="   flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main>Content</main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
